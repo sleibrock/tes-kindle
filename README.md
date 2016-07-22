f70c2f34d1c46353b33018c82da51e8115d1c011
Steven Leibrock <steven.leibrock@gmail.com>
2016-07-22 18:54:16 -0400

    First run of book creation

--- README.md
+++ README.md
@@ -7,12 +7,17 @@ enjoy it from your Amazon® Kindle™!
 # What is it?
 
 A project aimed at converting Elder Scrolls books, notes, journals, religious 
-texts and collections (lore) into the Kindle™ OBI format.
+texts and collections (lore) into the Kindle™ MOBI format.
 
 # Required
 
 * Python 3
 * Beautiful Soup 4
 * Requests
-* Kindlegen binary
+* [Kindlegen](https://www.amazon.com/gp/feature.html?docId=1000765211) binary
 * (Testing) A Kindle™ 
+
+# Credits
+
+Credits go to the [Imperial Library](http://www.imperial-library.info)
+for hosting user-submitted data from all Elder Scrolls games.
--- test-lusty.py
+++ test-lusty.py
@@ -3,15 +3,15 @@
 
 from bs4 import BeautifulSoup
 from requests import get as re_get
-from xml.etree import ElementTree
+from xml import etree as ET
 
-maid_url = "http://www.imperial-library.info/content/lusty-argonian-maid"
+base_url = "http://www.imperial-library.info"
+maid_url = "/content/lusty-argonian-maid"
 
 def get_bs(url):
-    return BeautifulSoup(re_get(url).text, 'html.parser')
+    return BeautifulSoup(re_get("{}{}".format(base_url, url)).text, 'html.parser')
 
 bs = get_bs(maid_url)
-
 
 # polishing spear begins below
 
@@ -21,9 +21,39 @@ bs = get_bs(maid_url)
 
 volumes_listing = bs.find_all("div", class_="book-navigation")
 
+search_items = list()
 if volumes_listing:
-    print("There's volumes!")
+    search_hrefs = [x.a["href"] for x in volumes_listing[0].find_all('li')]
+    search_items = [get_bs(href) for href in search_hrefs]
 else:
-    print("There isn't")
+    search_hrefs = [maid_url]
+    search_items = [bs]
 
+# Gather all the paragraphs of a given bs item
+def gather_text(bs):
+    node_root = bs.find_all("div", class_="node-content")
+    paras = [x for x in node_root[1].find_all("p")]
+    return paras
+
+def craft_fname(uri):
+    """
+    Turn a /path/to/text string into a text.html string
+    """
+    return "{}.html".format(uri.split("/").pop())
+
+def clean_s
+
+for uri, bs in zip(search_hrefs, search_items):
+    with open(craft_fname(uri), "w") as f:
+        f.write("<html>")
+        f.write("<head>")
+        f.write("<title>{}</title>".format("x d"))
+        f.write("<meta author='author name'>")
+        f.write("<meta charset='utf-8'>")
+        f.write("</head><body>")
+        for para in gather_text(bs):
+            f.write("{}\n".format(str(para)))
+        f.write("</body></html>")
+
+print("Done")
 
