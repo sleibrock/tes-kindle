79632fe0a780c5ecad3956f1ac4f7d7a707239e2
Steven Leibrock <steven.leibrock@gmail.com>
2016-07-23 17:36:53 -0400

    Finished product

--- README.md
+++ README.md
@@ -9,6 +9,10 @@ enjoy it from your Amazon® Kindle™!
 A project aimed at converting Elder Scrolls books, notes, journals, religious 
 texts and collections (lore) into the Kindle™ MOBI format.
 
+# Just gimme the books
+
+Okay, [here you go](https://drive.google.com/open?id=0B2tYExty6eSAZUx6U3F1VkZON2c).
+
 # Required
 
 * Python 3
@@ -16,6 +20,16 @@ texts and collections (lore) into the Kindle™ MOBI format.
 * Requests
 * [Kindlegen](https://www.amazon.com/gp/feature.html?docId=1000765211) binary
 * (Testing) A Kindle™ 
+* (Optional) Virtualenv
+
+# Setup
+
+It's as simple as cloning, installing packages, and running the script with 
+Python 3.
+
+But *please* don't do this as this will request so much information from the 
+Imperial Library repeatedly. Instead, just download all the desired books 
+from the [archive I uploaded above](https://drive.google.com/open?id=0B2tYExty6eSAZUx6U3F1VkZON2c).
 
 # Credits
 
--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
-from bs4 import BeautifulSoup as BS
-from subprocess import call as s_call
-from requests import get as re_get
-from os import mkdir, listdir
-from os.path import join, exists
+from bs4 import BeautifulSoup as BS   # parser
+from subprocess import call as s_call # sub-system call
+from requests import get as re_get    # fetch data from URI
+from os import mkdir, listdir         # list and make directories
+from os.path import join, exists      # check path exists, join path strings
+from shutil import move as fmove      # file moving
+from string import printable as printable_chrs # printable characters
+from sys import argv                  # obvious
 
-BOOK_DIR    = "out"
-BOOK_NAME   = "[{0}]-{1}.html"
+# Output settings
+OUTDIR      = "out"
+OUTNAME     = "{1}.mobi"
 ROOT_URL    = "http://www.imperial-library.info"
 
 # Really crappy check to see if we have a local bin or not 
 KGEN_BIN    = "kindlegen" if "kindlegen" not in listdir(".") else "./kindlegen"
 
+# All games organized by section
 SOURCE_URLS = {
     "daggerfall"  : "/books/daggerfall/by-title",
     "battlespire" : "/books/battlespire/by-title",
@@ -26,6 +31,8 @@ SOURCE_URLS = {
     }
 
 # We have to re-write an OPF file with each book's details 
+# Kindlegen uses the OPF XML file as a way of writing info about the book
+# No point in creating a complex ETree to express this when I'm changing two things
 OPF_BLOB    = """
 <?xml version="1.0" encoding="iso-8859-1"?>
 <package unique-identifier="uid" xmlns:opf="http://www.idpf.org/2007/opf" xmlns:asd="http://www.idpf.org/asdfaf">
@@ -39,20 +46,21 @@ OPF_BLOB    = """
         </dc-metadata>
     </metadata>
     <manifest>
-        <item id="text" media-type="text/x-oeb1-document" href="index.html"></item>
+        <item id="text" media-type="text/x-oeb1-document" href="output.html"></item>
     </manifest>
     <spine toc="ncx">
         <itemref idref="text"/>
     </spine>
     <guide>
-        <reference type="text" title="Book" href="index.html"/>
+        <reference type="text" title="Book" href="output.html"/>
     </guide>
 </package>
 """
 
+# The HTML of the book, doesn't have to be very complex
 HTML_BLOB = """
 <html>
-<head><style>.rtecenter{text-align:center;}</style></head>
+<head><style>.rtecenter{{text-align:center;}}</style></head>
 <body>
 {0}
 </body>
@@ -60,68 +68,135 @@ HTML_BLOB = """
 """
 
 def craft_url(url):
+    """
+    Return a URL using the ROOT_RUL variable at top-level
+    """
     return "{}{}".format(ROOT_URL, url)
 
 def get_bs(url):
-    return BS(re_get(craft_url(url)).text, 'html.parser')
+    """
+    Return a parser of the given URL fragment
+    /content/something-id -> BS(ROOT_URL/content/something-id)
+    Applies a ton of filtering/replacing so we don't get bad characters
+    """
+    text = re_get(craft_url(url)).text
+    text = "".join([c for c in text if c in printable_chrs])
+    text = text.replace("\n", " ").replace("\xa0", " ")
+    return BS(text, 'html.parser')
 
 def craft_fname(section, url):
-    return BOOK_NAME.format(section, url.split("/").pop())
+    """
+    Craft a filename for moving the MOBI file
+    """
+    return OUTNAME.format(section, url.split("/").pop())
 
-def isnt_empty(tag):
-    if tag.text.strip() == "\xa0" or tag.text.replace("\xa0", " ").strip() == "":
-        return False
-    return True
+def run_kindlegen():
+    """
+    Call the kindlegen compiler to create an output MOBI blob
+    The blob then has to be moved by the program to a folder
+    """
+    return s_call([KGEN_BIN, "-o", "output.mobi", "book.opf"])
 
-def run_kindlegen(section, url):
-    return call(["kindlegen", "-o", "something.mobi", "data.html"])
+def get_title(bs, game):
+    """
+    Return the title of the given book BS
+    Luckily it seems to be the only H1 tag in the BS
+    """
+    return "[{0}] {1}".format(game.capitalize(), bs.h1.text)
+
+def get_author(bs):
+    """
+    Find the author name, which exists in a strange div next to some strange 
+    characters, next to a text label of "Author", so we must remove 
+    all of that to extract the final Author name
+    """
+    auth = bs.find_all("div", class_="field-item odd")
+    if not len(auth):
+        return ""
+    auth = auth[0].text.replace("Author:", "")
+    auth = "".join([c for c in auth if c in printable_chrs]).strip()
+    return auth
 
 def gather_text(bs):
+    """
+    Collect all paragraphs under the node-content div
+    These can be joined later but for now, only return a list
+    '\xa0' gets inserted by BS so we have to clear it after it parses
+    """
     node_root = bs.find_all("div", class_="node-content")
-    paras = [x for x in node_root[1].find_all("p")]
-    return paras
+    return [str(p).replace("\xa0", " ") for p in node_root[1].find_all("p")]
+
+def to_book(game, url):
+    """
+    Main book algorithm
+    1. first detect if a given URL is a listing of multiple books
+    2. then pass the URL(s) to a processing section where we write files
+    3. Run kindlegen on the newly created html/opf blobs
+    4. Move the MOBI output into it's respective folder with it's new name
+    """
+    raw_bs = get_bs(url)
+    vol_ls = raw_bs.find_all("div", class_="book-navigation")
+
+    if vol_ls: # this page contains listing for multiple books
+        s_href = [x.a["href"] for x in vol_ls[0].find_all("li")]
+        s_bses = [get_bs(href) for href in s_href]
+    else:      # this page is the only book
+        s_href = [url]
+        s_bses = [raw_bs]
+
+    # Write HTML, OPF, and run the compiler
+    for url, bs in zip(s_href, s_bses):
+        with open("output.html", "w") as hf:
+                hf.write(HTML_BLOB.format("\n".join(gather_text(bs))))
+
+        with open("book.opf", "w") as of:
+            of.write(OPF_BLOB.format(get_title(bs, game), get_author(bs)))
+
+        try:
+            run_kindlegen() # run the compiler
+            fmove("output.mobi", join(OUTDIR, game, craft_fname(game, url)))
+        except Exception as e:
+            print("Error occurred: {}".format(str(e)))
+    return True 
 
 def main(*args, **kwargs):
+    """
+    Main program to set up the processing
+    """
     book_hrefs = list() # list to collect all HREFs gathered
 
     # first check whether we have an output directory
-    if not exists(BOOK_DIR):
+    if not exists(OUTDIR):
         print("Creating book output directory")
-        mkdir(BOOK_DIR)
+        mkdir(OUTDIR)
         print("Creating sub-directories")
         for game in SOURCE_URLS.keys():
-            mkdir(join(BOOK_DIR, game))
-        print("Done")
+            mkdir(join(OUTDIR, game))
+        print("Done creating directories")
 
+    # Build a list of URLs to scan from each index
     for game, source_url in SOURCE_URLS.items():
-        print("Running downloads on {}...".format(source_url))
+        print("Downloading index of {}...".format(source_url))
         index = get_bs(source_url) # get the BS of the current index
         book_hrefs.extend([(game, x['href']) for x in index.find_all("div", class_="view-content")[0].find_all("a")])
     print("Total count: {}".format(len(book_hrefs)))
 
-    # Download each book page's data, check if it contains multiple books
-    # Export the data to an HTML/OPF file, render each one one-by-one to 
-    # it's respective folder
+    # Process each URL and it's game to the to_book() function
+    count = 0
     for game, href in book_hrefs:
-        raw_bs = get_bs(href)
-        vol_ls = raw_bs.find_all("div", class_="book-navigation")
-        if vol_ls:
-            s_href = [x.a["href"] for x in vol_ls[0].find_all('li')]
-            s_bses = [get_bs(href) for href in s_href]
-        else:
-            s_href = [href]
-            s_bses = [raw_bs]
+        print("Processing {}...".format(href))
+        to_book(game, href)
+        count += 1
+        print("{0} books processed".format(count))
 
-        for url, bs in zip(s_href, s_bses):
-            with open("output.html", "w") as hf:
-                f.write("test")
-
-            with open("book.opf", "w") as of:
-                f.write("test")
-
+    # end
     return None
 
+
 if __name__ == "__main__":
+    if "-t" in argv or "--test" in argv:
+        print("Testing")
+        quit()
     main()
 
 # end
